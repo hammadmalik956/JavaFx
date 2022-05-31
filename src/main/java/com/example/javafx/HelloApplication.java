f58a8ba3d3445ef40f0bc00b019a65416e3ce4e9
hammadmalik956 <hammadhameed956@gmail.com>
2022-05-31 17:33:27 +0500

    updation

--- src/main/java/com/example/javafx/HelloApplication.java
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HireTrainer.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("UserPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 740, 540);
         stage.setTitle("GYM MANAGEMENT SYSTEM");
         stage.setScene(scene);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.javafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.sql;
 
 
     opens com.example.javafx to javafx.fxml;
